672d413863415350f03d651f34cabffafb1a561e
SZN <sznckz90@gmail.com>
2026-06-12 00:42:59 +0530

    Add files via upload
    Signed-off-by: SZN <sznckz90@gmail.com>

--- shared/constants.ts
+++ shared/constants.ts
@@ -8,11 +8,11 @@ export const APP_COLORS = {
 
 /**
  * Convert PAD to USD
- * @param padAmount - Amount in PAD
+ * @param powAmount - Amount in PAD
  * @returns Amount in USD (PAD / 10,000,000)
  */
-export function padToUSD(padAmount: number | string): number {
-  const numValue = typeof padAmount === 'string' ? parseFloat(padAmount) : padAmount;
+export function padToUSD(powAmount: number | string): number {
+  const numValue = typeof powAmount === 'string' ? parseFloat(powAmount) : powAmount;
   return numValue / PAD_TO_USD;
 }
 
