7ff203cb0f40d45f42ee635a9eac117923245875
SZN <sznckz90@gmail.com>
2026-06-29 19:33:49 +0530

    Add files via upload
    Signed-off-by: SZN <sznckz90@gmail.com>

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -93,11 +93,9 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <div className="h-screen w-full flex flex-col bg-black overflow-hidden">
-      {!showSeasonEnd && <Header />}
-
       <div
         className="flex-1 overflow-y-auto overflow-x-hidden"
-        style={{ paddingBottom: "100px", paddingTop: "64px" }}
+        style={{ paddingBottom: "100px", paddingTop: "0px" }}
       >
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
